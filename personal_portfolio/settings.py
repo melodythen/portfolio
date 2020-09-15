ed89cfba0e62d5174e28e703802653ecd42eb159
melodythen <melody.then@mail.mcgill.ca>
2020-09-15 13:27:52 -0400

    fixed security issue

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yra*&nrn$6p2wak+gnu$)eq&^yb$dvx=iofv$j-v-l%#ioiou!'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
