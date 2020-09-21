cfe332964e9717a1d38e7aa84e8bafb6eaafda0c
Melody Then <melody.then@mail.mcgill.ca>
2020-09-21 16:22:55 -0400

    finished project page

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -81,7 +81,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
 }
 
 
